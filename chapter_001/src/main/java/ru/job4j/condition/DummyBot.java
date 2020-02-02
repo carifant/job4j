d7bec2032583840b8b36857855f08f1cd31a4b1e
carifant <carifant@mail.ru>
2020-02-02 19:16:33 +0300

    4.1. Глупый бот.[#227975]

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,22 @@
+package ru.job4j.condition;
+
+public class DummyBot {
+
+    public static void main(String[] args) {
+  DummyBot db = new DummyBot();
+  System.out.println(DummyBot.answer("Привет, Бот."));
+  System.out.println(DummyBot.answer("Пока."));
+  System.out.println(DummyBot.answer("Сколько времени?"));
+    }
+
+    public static String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            return "Привет, умник.";
+        } else if ("Пока.".equals(question)) {
+            return "До скорой встречи.";
+        } else
+            return rsl;
+
+    }
+}
