a481f7e3ef862076de0cadd0ff4802d0672edd2a
carifant <carifant@mail.ru>
2020-02-05 20:37:10 +0300

    5.8. JavaDoc. Code convention[#228060]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,22 +1,55 @@
 package ru.job4j.calculator;
 
+/**
+ * Класс для вычисления арифмитических операций. + - * /.
+ * @author Alexei Katyshev
+ * @since 27.01.20
+ * @version 1.0
+ *
+ */
+
 public class Calculator {
+
+    /**
+     * Method can add two numbers.
+     * @param first - the first number.
+     * @param second - the second number.
+     */
     public static void add(double first, double second) {
         double result =  first + second;
         System.out.println(first + "+" + second + " = " + result);
     }
+    /**
+     * Method can divide  numbers.
+     * @param first - the first number.
+     * @param second - the second number.
+     */
     public static void div(double first, double second) {
         double result =  first / second;
         System.out.println(first + "/" + second + " = " + result);
     }
+    /**
+     * Method can multiply  numbers.
+     * @param first - the first number.
+     * @param second - the second number.
+     */
     public static void multiply(double first, double second) {
         double result =  first * second;
         System.out.println(first + "*" + second + " = " + result);
     }
+    /**
+     * Method can subtract  numbers.
+     * @param first - the first number.
+     * @param second - the second number.
+     */
     public static void subtract(double first, double second) {
         double result =  first - second;
         System.out.println(first + "-" + second + " = " + result);
     }
+    /**
+     * main method .
+     * @param args - args.
+     */
     public static void main(String[] args) {
 
         add(1,1);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Katyshev Alexei (carifant@mail.ru)
+ * @version $Id$
+ * @since 05.02.2020
+ */
+package ru.job4j.calculator;
+
+
