740008ec2b0958606b5274dea2a9330f9268dcd1
carifant <carifant@mail.ru>
2020-02-06 20:46:22 +0300

    6.2.1. Выход за границу массива.[#228008]

--- chapter_001/src/main/java/ru/job4j/array/AlgoArray.java
+++ chapter_001/src/main/java/ru/job4j/array/AlgoArray.java
@@ -6,7 +6,7 @@ public class AlgoArray {
         int[] array = new int[] {5, 3, 2, 1, 4};
         int temp = array[0];
         array[0] = array[3];
-        array[3] = temp; 
+        array[3] = temp;
         temp = array[1];
         array[1] = array[2];
         array[2] = temp;
