e3ee6570cce557577ea6100ae58f401f3bcfbaa0
carifant <carifant@mail.ru>
2020-02-06 19:26:45 +0300

    6.0.3. Массивы и цикл for.[#228005]

---
+++ chapter_001/src/main/java/array/ArrayLoop.java
@@ -0,0 +1,14 @@
+package array;
+
+public class ArrayLoop {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i * 2 + 3;
+        }
+        for (int j = 0; j < numbers.length; j++) {
+            System.out.println(numbers[j]);
+        }
+    }
+}
