f54edff5e9dd8aec9a2b52619b39671282cf63b8
carifant <carifant@mail.ru>
2020-02-09 16:19:24 +0300

    6.8. Дефрагментация массива.[#228000]

---
+++ chapter_001/src/test/java/ru/job4j/array/DefragmentTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.array;
+
+
+    import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+    public class DefragmentTest {
+
+        @Test
+        public void notFirstNull() {
+            String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+            String[] compressed = Defragment.compress(input);
+            String[] expected = {"I", "wanna", "be", "compressed", null, null, null};
+            assertThat(compressed, is(expected));
+        }
+
+        @Test
+        public void firstNull() {
+            String[] input = {null, "I", "wanna", null, "be", null, "compressed"};
+            String[] compressed = Defragment.compress(input);
+            String[] expected = {"I", "wanna", "be", "compressed", null, null, null};
+            assertThat(compressed, is(expected));
+        }
+        @Test
+        public void aLotOfNull() {
+            String[] input = {null, "I", "am", null , "going", " to be", null, null, "a" ,  "programmist"};
+            String[] compressed = Defragment.compress(input);
+            String[] expected = {"I", "am", "going", " to be", "a", "programmist", null, null, null, null};
+            assertThat(compressed, is(expected));
+        }
+    }
+
