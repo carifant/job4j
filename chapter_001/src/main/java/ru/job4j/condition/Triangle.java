40c827336f20e19b5e6d6f0fafd98271ecabcc1c
carifant <carifant@mail.ru>
2020-02-19 18:59:59 +0300

    2.2. Рефакторинг - Площадь треугольника.[#228148]

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -2,7 +2,40 @@ package ru.job4j.condition;
 
 public class Triangle {
 
-    public static boolean exist(double ab, double ac, double bc) {
-        return ab + ac > bc && ac + bc > ab && ab + bc > ac;
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
+
+    public double period(Triangle triangle) {
+        return (triangle.first.distance(triangle.second) + triangle.first.distance(triangle.third)
+                + triangle.second.distance(triangle.third)) / 2;
+    }
+
+
+    public double area(Triangle triangle) {
+        double rsl = -1;
+        double p = period(triangle);
+        if (this.exist(triangle)) {
+            return Math.sqrt(p * (p - triangle.first.distance(triangle.second)) *
+                    (p - triangle.first.distance(triangle.third)) * (p - triangle.second.distance(triangle.third)));
+
+        }
+        return rsl;
+    }
+
+
+    public boolean exist(Triangle triangle) {
+        return (triangle.first.distance(triangle.second) + triangle.first.distance(triangle.third) > triangle.second.distance(triangle.third) &&
+                triangle.second.distance(triangle.third) + triangle.first.distance(triangle.third) > triangle.first.distance(triangle.second) &&
+                triangle.first.distance(triangle.second) + triangle.second.distance(triangle.third) > triangle.first.distance(triangle.third));
+
+
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -9,13 +9,21 @@ public class TriangleTest {
 
     @Test
     public void whenExist() {
-        boolean result = Triangle.exist(2.0, 2.0, 2.0);
+        Point point = new Point(0, 0);
+        Point point2 = new Point(0, 2);
+        Point point3 = new Point(1, 2);
+        Triangle t = new Triangle(point, point2, point3);
+        boolean result = t.exist(t);
         assertThat(result, is(true));
     }
 
     @Test
     public void whenNotExist() {
-        boolean result = Triangle.exist(5.0, 2.0, 2.0);
+        Point point = new Point(10, 1);
+        Point point2 = new Point(1, 2);
+        Point point3 = new Point(1, 2);
+        Triangle t = new Triangle(point, point2, point3);
+        boolean result = t.exist(t);
         assertThat(result, is(false));
     }
 }
