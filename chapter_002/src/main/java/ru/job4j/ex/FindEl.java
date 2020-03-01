c2278a65795713ae8cde9f5b1460c01d020791bc
carifant <carifant@mail.ru>
2020-03-01 14:28:09 +0300

    0.4. Пользовательские исключения.[#228140]

---
+++ chapter_002/src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,27 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (String x : value) {
+            if (x.equals(key)) {
+                System.out.println("Элемент найден");
+                rsl = Integer.parseInt(x);
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Элемент не был найден в массиве");
+
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] names = {"Василий", "Дмитрий", "Александр"};
+        try {
+            indexOf(names, "Андрей");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
