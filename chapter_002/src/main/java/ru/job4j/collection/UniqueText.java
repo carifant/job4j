70754ab5157cef8c7ccc289d3eaa76ef2bea2633
carifant <carifant@mail.ru>
2020-03-15 13:56:49 +0300

    2. Идентичные тексты.[#228099]

---
+++ chapter_002/src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String x : origin) {
+            check.add(x);
+        }
+        for (String j : text) {
+            if (!check.contains(j)) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+}
