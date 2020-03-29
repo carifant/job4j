e134c575ce5d654c05911860b9ecc5018ef28669
carifant <carifant@mail.ru>
2020-03-29 20:18:05 +0300

    В классе StartUI заменить вывод в консоль на Consumer.[#227943]

--- chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
+++ chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,6 +1,11 @@
 package ru.job4j.search;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public class PhoneDictionary {
     private ArrayList<Person> persons = new ArrayList<Person>();
@@ -9,21 +14,22 @@ public class PhoneDictionary {
         this.persons.add(person);
     }
 
-    /**
-     * Вернуть список всех пользователей, который содержат key в любых полях.
-     *
-     * @param key Ключ поиска.
-     * @return Список подощедщих пользователей.
-     */
     public ArrayList<Person> find(String key) {
+        Predicate<Person> combine = new Predicate<Person>() {
+            @Override
+            public boolean test(Person person) {
+                ArrayList<String> list = new ArrayList<>();
+                list.add(person.getSurname());
+                list.add(person.getAddress());
+                list.add(person.getName());
+                list.add(person.getPhone());
+                return list.contains(key);
+            }
+        };
         ArrayList<Person> result = new ArrayList<>();
-        for (int i = 0; i < persons.size(); i++) {
-            String temp = persons.get(i).getSurname();
-            String temp2 = persons.get(i).getAddress();
-            String temp3 = persons.get(i).getName();
-            String temp4 = persons.get(i).getPhone();
-            if (temp.contains(key) || temp2.contains(key) || temp3.contains(key) || temp4.contains(key)) {
-                result.add(persons.get(i));
+        for (Person person : persons) {
+            if (combine.test(person)) {
+                result.add(person);
             }
         }
         return result;
