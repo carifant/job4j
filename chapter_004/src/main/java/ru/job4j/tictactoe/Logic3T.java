a250544269ba7e0844797d75b49d153433cceff6
carifant <carifant@mail.ru>
2020-04-17 08:15:04 +0300

    Закомментил крестики нолики

--- chapter_004/src/main/java/ru/job4j/tictactoe/Logic3T.java
+++ chapter_004/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -1,42 +1,42 @@
-package ru.job4j.tictactoe;
-
-import java.util.Arrays;
-import java.util.function.Predicate;
-import java.util.stream.Stream;
-
-public class Logic3T {
-    private final Figure3T[][] table;
-
-    public Logic3T(Figure3T[][] table) {
-        this.table = table;
-    }
-
-    public boolean fillBy(Predicate<Figure3T> predicate, int startX, int startY, int deltaX, int deltaY) {
-        boolean result = true;
-        for (int index = 0; index != this.table.length; index++) {
-            Figure3T cell = this.table[startX][startY];
-            startX += deltaX;
-            startY += deltaY;
-            if (!predicate.test(cell)) {
-                result = false;
-                break;
-            }
-        }
-        return result;
-    }
-
-    public boolean isWin(Predicate<Figure3T> condition) {
-        return this.fillBy(condition, 0, 0, 1, 0)
-                || this.fillBy(condition, 0, 1, 1, 0)
-                || this.fillBy(condition, 0, 2, 1, 0)
-                || this.fillBy(condition, 0, 0, 0, 1)
-                || this.fillBy(condition, 1, 0, 0, 1)
-                || this.fillBy(condition, 2, 0, 0, 1)
-                || this.fillBy(condition, 0, 0, 1, 1)
-                || this.fillBy(condition, 0, 2, 1, -1);
-    }
-
-    public boolean hasGap() {
-        return Arrays.stream(table).flatMap(Arrays::stream).anyMatch(x -> !x.hasMarkX() && !x.hasMarkO());
-    }
-}
+//package ru.job4j.tictactoe;
+//
+//import java.util.Arrays;
+//import java.util.function.Predicate;
+//import java.util.stream.Stream;
+//
+//public class Logic3T {
+//    private final Figure3T[][] table;
+//
+//    public Logic3T(Figure3T[][] table) {
+//        this.table = table;
+//    }
+//
+//    public boolean fillBy(Predicate<Figure3T> predicate, int startX, int startY, int deltaX, int deltaY) {
+//        boolean result = true;
+//        for (int index = 0; index != this.table.length; index++) {
+//            Figure3T cell = this.table[startX][startY];
+//            startX += deltaX;
+//            startY += deltaY;
+//            if (!predicate.test(cell)) {
+//                result = false;
+//                break;
+//            }
+//        }
+//        return result;
+//    }
+//
+//    public boolean isWin(Predicate<Figure3T> condition) {
+//        return this.fillBy(condition, 0, 0, 1, 0)
+//                || this.fillBy(condition, 0, 1, 1, 0)
+//                || this.fillBy(condition, 0, 2, 1, 0)
+//                || this.fillBy(condition, 0, 0, 0, 1)
+//                || this.fillBy(condition, 1, 0, 0, 1)
+//                || this.fillBy(condition, 2, 0, 0, 1)
+//                || this.fillBy(condition, 0, 0, 1, 1)
+//                || this.fillBy(condition, 0, 2, 1, -1);
+//    }
+//
+//    public boolean hasGap() {
+//        return Arrays.stream(table).flatMap(Arrays::stream).anyMatch(x -> !x.hasMarkX() && !x.hasMarkO());
+//    }
+//}
--- chapter_004/src/test/java/ru/job4j/tictactoe/Logic3TTest.java
+++ chapter_004/src/test/java/ru/job4j/tictactoe/Logic3TTest.java
@@ -1,102 +1,102 @@
-package ru.job4j.tictactoe;
-
-import org.junit.Ignore;
-import org.junit.Test;
-
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
-
-@Ignore
-public class Logic3TTest {
-
-    @Test
-    public void whenHasXWinner() {
-        Figure3T[][] table = {
-                {new Figure3T(true, false), new Figure3T(), new Figure3T()},
-                {new Figure3T(), new Figure3T(true, false), new Figure3T()},
-                {new Figure3T(), new Figure3T(), new Figure3T(true, false)},
-        };
-        Logic3T login = new Logic3T(table);
-        assertThat(login.isWin(Figure3T::hasMarkX), is(true));
-    }
-
-
-    @Test
-    public void whenNotFill() {
-        Figure3T[][] table = {
-                {new Figure3T(true, false), new Figure3T(), new Figure3T()},
-                {new Figure3T(), new Figure3T(), new Figure3T()},
-                {new Figure3T(), new Figure3T(), new Figure3T()},
-        };
-        Logic3T login = new Logic3T(table);
-        assertThat(login.isWin(Figure3T::hasMarkX), is(false));
-        assertThat(login.isWin(Figure3T::hasMarkX), is(false));
-    }
-
-    @Test
-    public void whenHasXHorizontalWinner() {
-        Figure3T[][] table = {
-                {new Figure3T(), new Figure3T(), new Figure3T()},
-                {new Figure3T(true, false), new Figure3T(true, false), new Figure3T(true, false)},
-                {new Figure3T(), new Figure3T(), new Figure3T()},
-        };
-        Logic3T login = new Logic3T(table);
-        assertThat(login.isWin(Figure3T::hasMarkX), is(true));
-    }
-
-    @Test
-    public void whenHasXVerticalWinner() {
-        Figure3T[][] table = {
-                {new Figure3T(), new Figure3T(true, false), new Figure3T()},
-                {new Figure3T(), new Figure3T(true, false), new Figure3T()},
-                {new Figure3T(), new Figure3T(true, false), new Figure3T()},
-        };
-        Logic3T login = new Logic3T(table);
-        assertThat(login.isWin(Figure3T::hasMarkX), is(true));
-    }
-
-    @Test
-    public void whenHasXBackDiagonalWinner() {
-        Figure3T[][] table = {
-                {new Figure3T(), new Figure3T(), new Figure3T(true, false)},
-                {new Figure3T(), new Figure3T(true, false), new Figure3T()},
-                {new Figure3T(true, false), new Figure3T(), new Figure3T()},
-        };
-        Logic3T login = new Logic3T(table);
-        assertThat(login.isWin(Figure3T::hasMarkX), is(true));
-    }
-
-    @Test
-    public void whenHasOWinner() {
-        Figure3T[][] table = {
-                {new Figure3T(false, true), new Figure3T(), new Figure3T()},
-                {new Figure3T(false, true), new Figure3T(), new Figure3T()},
-                {new Figure3T(false, true), new Figure3T(), new Figure3T()},
-        };
-        Logic3T login = new Logic3T(table);
-        assertThat(login.isWin(Figure3T::hasMarkO), is(true));
-    }
-
-    @Test
-    public void whenHasGap() {
-        Figure3T[][] table = {
-                {new Figure3T(true, false), new Figure3T(), new Figure3T()},
-                {new Figure3T(), new Figure3T(true, false), new Figure3T()},
-                {new Figure3T(), new Figure3T(), new Figure3T(true, false)},
-        };
-        Logic3T login = new Logic3T(table);
-        assertThat(login.hasGap(), is(true));
-    }
-
-    @Test
-    public void whenUnHasGap() {
-        Figure3T[][] table = {
-                {new Figure3T(true, false), new Figure3T(true, false), new Figure3T(true, false)},
-                {new Figure3T(true, false), new Figure3T(true, false), new Figure3T(true, false)},
-                {new Figure3T(true, false), new Figure3T(true, false), new Figure3T(true, false)},
-        };
-        Logic3T login = new Logic3T(table);
-        assertThat(login.hasGap(), is(false));
-    }
-}
-
+//package ru.job4j.tictactoe;
+//
+//import org.junit.Ignore;
+//import org.junit.Test;
+//
+//import static org.hamcrest.core.Is.is;
+//import static org.junit.Assert.assertThat;
+//
+//@Ignore
+//public class Logic3TTest {
+//
+//    @Test
+//    public void whenHasXWinner() {
+//        Figure3T[][] table = {
+//                {new Figure3T(true, false), new Figure3T(), new Figure3T()},
+//                {new Figure3T(), new Figure3T(true, false), new Figure3T()},
+//                {new Figure3T(), new Figure3T(), new Figure3T(true, false)},
+//        };
+//        Logic3T login = new Logic3T(table);
+//        assertThat(login.isWin(Figure3T::hasMarkX), is(true));
+//    }
+//
+//
+//    @Test
+//    public void whenNotFill() {
+//        Figure3T[][] table = {
+//                {new Figure3T(true, false), new Figure3T(), new Figure3T()},
+//                {new Figure3T(), new Figure3T(), new Figure3T()},
+//                {new Figure3T(), new Figure3T(), new Figure3T()},
+//        };
+//        Logic3T login = new Logic3T(table);
+//        assertThat(login.isWin(Figure3T::hasMarkX), is(false));
+//        assertThat(login.isWin(Figure3T::hasMarkX), is(false));
+//    }
+//
+//    @Test
+//    public void whenHasXHorizontalWinner() {
+//        Figure3T[][] table = {
+//                {new Figure3T(), new Figure3T(), new Figure3T()},
+//                {new Figure3T(true, false), new Figure3T(true, false), new Figure3T(true, false)},
+//                {new Figure3T(), new Figure3T(), new Figure3T()},
+//        };
+//        Logic3T login = new Logic3T(table);
+//        assertThat(login.isWin(Figure3T::hasMarkX), is(true));
+//    }
+//
+//    @Test
+//    public void whenHasXVerticalWinner() {
+//        Figure3T[][] table = {
+//                {new Figure3T(), new Figure3T(true, false), new Figure3T()},
+//                {new Figure3T(), new Figure3T(true, false), new Figure3T()},
+//                {new Figure3T(), new Figure3T(true, false), new Figure3T()},
+//        };
+//        Logic3T login = new Logic3T(table);
+//        assertThat(login.isWin(Figure3T::hasMarkX), is(true));
+//    }
+//
+//    @Test
+//    public void whenHasXBackDiagonalWinner() {
+//        Figure3T[][] table = {
+//                {new Figure3T(), new Figure3T(), new Figure3T(true, false)},
+//                {new Figure3T(), new Figure3T(true, false), new Figure3T()},
+//                {new Figure3T(true, false), new Figure3T(), new Figure3T()},
+//        };
+//        Logic3T login = new Logic3T(table);
+//        assertThat(login.isWin(Figure3T::hasMarkX), is(true));
+//    }
+//
+//    @Test
+//    public void whenHasOWinner() {
+//        Figure3T[][] table = {
+//                {new Figure3T(false, true), new Figure3T(), new Figure3T()},
+//                {new Figure3T(false, true), new Figure3T(), new Figure3T()},
+//                {new Figure3T(false, true), new Figure3T(), new Figure3T()},
+//        };
+//        Logic3T login = new Logic3T(table);
+//        assertThat(login.isWin(Figure3T::hasMarkO), is(true));
+//    }
+//
+//    @Test
+//    public void whenHasGap() {
+//        Figure3T[][] table = {
+//                {new Figure3T(true, false), new Figure3T(), new Figure3T()},
+//                {new Figure3T(), new Figure3T(true, false), new Figure3T()},
+//                {new Figure3T(), new Figure3T(), new Figure3T(true, false)},
+//        };
+//        Logic3T login = new Logic3T(table);
+//        assertThat(login.hasGap(), is(true));
+//    }
+//
+//    @Test
+//    public void whenUnHasGap() {
+//        Figure3T[][] table = {
+//                {new Figure3T(true, false), new Figure3T(true, false), new Figure3T(true, false)},
+//                {new Figure3T(true, false), new Figure3T(true, false), new Figure3T(true, false)},
+//                {new Figure3T(true, false), new Figure3T(true, false), new Figure3T(true, false)},
+//        };
+//        Logic3T login = new Logic3T(table);
+//        assertThat(login.hasGap(), is(false));
+//    }
+//}
+//
